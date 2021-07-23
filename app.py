5e9c9e1232b84de08d1df60e4b6014ddf0cbd82f
Thabo Setsubi <thabosetsubi3@gmail.com>
2021-07-23 10:50:35 +0200

    Update

---
+++ app.py
@@ -0,0 +1,50 @@
+from flask import Flask, render_template, request
+import sqlite3
+
+
+def init_sqlite_db():
+    conn = sqlite3.connect('database.db')
+    print("Opened database successfully")
+
+    conn.execute("CREATE TABLE IF NOT EXISTS students (name TEXT, addr TEXT, city TEXT, pin TEXT)")
+    print("Table created successfully")
+    conn.close()
+
+
+init_sqlite_db()
+
+
+app = Flask(__name__)
+
+
+@app.route('/enter-new/')
+def enter_new_students():
+    return render_template("student.html")
+
+
+@app.route('/add-new-record/', methods=['POST'])
+def add_new_record():
+    if request.method == "POST":
+        try:
+            name = request.form['name']
+            addr = request.form['add']
+            city = request.form['city']
+            pin = request.form['pin']
+
+            with sqlite3.connect('database.db') as con:
+                cur = con.cursor()
+                cur.execute("INSERT INTO students (name, addr, city, pin) VALUES (?, ?, ?, ?)", (name, addr, city, pin))
+                con.commit()
+                msg = "Record successfully added"
+
+        except Exception as e:
+            con.rollback()
+            msg = "Error occurred in insert operation: " + str(e)
+
+        finally:
+            con.close()
+            return render_template('result.html', msg=msg)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
